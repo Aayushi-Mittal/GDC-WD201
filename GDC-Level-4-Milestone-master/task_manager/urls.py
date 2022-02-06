86aed0651654fd0ce183db9b463792ee677eb04b
Aayushi-Mittal <aayushimittal088@gmail.com>
2022-02-07 03:44:57 +0530

    Added endpoints for all_tasks, tasks, completed_tasks

--- GDC-Level-4-Milestone-master/task_manager/urls.py
+++ GDC-Level-4-Milestone-master/task_manager/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
+from tasks import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # Add all your views here
+    path("all_tasks/", views.all_tasks_view),
+    path("tasks/", views.tasks_view),
+    path("completed_tasks/", views.completed_tasks_view),
 ]
--- GDC-Level-4-Milestone-master/tasks/views.py
+++ GDC-Level-4-Milestone-master/tasks/views.py
@@ -1,1 +1,27 @@
 # Add all your views here
+from django.shortcuts import render
+from tasks import views
+
+# tasks = ["This is my first task"]
+# completed_tasks = ["I am done", "second done"]
+tasks = []
+completed_tasks = []
+
+
+def all_tasks_view(request):
+    return render(
+        request,
+        "all_tasks.html",
+        {
+            "tasks": tasks,
+            "completed_tasks": completed_tasks,
+        },
+    )
+
+
+def tasks_view(request):
+    return render(request, "tasks.html", {"tasks": tasks})
+
+
+def completed_tasks_view(request):
+    return render(request, "completed_tasks.html", {"completed_tasks": completed_tasks})
