d4e7734aa5b332f7046bc5a2ba190d1aff1cf47c
Aayushi-Mittal <aayushimittal088@gmail.com>
2022-02-21 17:37:01 +0530

    Implemented pagination

--- GDC-Level-6-Milestone-master/task_manager/urls.py
+++ GDC-Level-6-Milestone-master/task_manager/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 
-from tasks.views import add_task_view, delete_task_view, TaskView
+# from tasks import views
 
+from tasks.views import add_task_view, delete_task_view, GenericTaskView;
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("all_tasks/", all_tasks_view),
-    # path("completed_tasks/", completed_tasks_view),
-    path("tasks/", TaskView.as_view()),
+    # path("all_tasks/", views.all_tasks_view),
+    path("tasks/", GenericTaskView.as_view()),
+    # path("completed_tasks/", views.completed_tasks_view),
     path("add-task/", add_task_view),
     path("delete-task/<int:index>/", delete_task_view),
-    # path("complete_task/<int:index>/", complete_task_view),
+    # path("complete_task/<int:index>/", views.complete_task_view),
 ]
--- GDC-Level-6-Milestone-master/tasks/views.py
+++ GDC-Level-6-Milestone-master/tasks/views.py
@@ -20,44 +20,46 @@ from tasks.models import Task
 # def completed_tasks_view(request):
 #     return render(request, "completed_tasks.html", {"completed_tasks": Task.objects.filter(deleted=False).filter(completed=True)})
 
-class TaskView(View):
-
-    def get(self, request):
-        search_term=request.GET.get("search")
-        tasks=Task.objects.filter(deleted=False)
-        if search_term:
-            tasks=tasks.filter(title__icontains=search_term)
-        return render(request,"tasks.html", {"tasks":tasks})
-
-    def post(self, request):
-        pass
 
 class GenericTaskView(ListView):
 
     queryset = Task.objects.filter(deleted=False)
-    templatname="tasks.html"
-    content_object_name="tasks"
-    paginate_by=5
+    template_name = "tasks.html"
+    context_object_name = "tasks"
+    paginate_by = 5
 
     def get_queryset(self):
-        search_term=self.request.GET.get("search")
-        task=Task.objects.filter(deleted=False)
+        search_term = self.request.GET.get("search")
+        tasks = Task.objects.filter(deleted=False)
         if search_term:
-            tasks=tasks.filter(title__icontains=search_term)
+            tasks = tasks.filter(title__icontains=search_term)
         return tasks
+
+
+class TaskView(View):
+    def get(self, request):
+        search_term = request.GET.get("search")
+        tasks = Task.objects.filter(deleted=False)
+        if search_term:
+            tasks = tasks.filter(title__icontains=search_term)
+        return render(request, "tasks.html", {"tasks": tasks})
+
+    def post(self, request):
+        pass
+
 
 def add_task_view(request):
     task_value = request.GET.get("task")
     task_obj = Task(title=task_value)
     task_obj.save()
-    return HttpResponseRedirect("/all_tasks")
+    return HttpResponseRedirect("/tasks")
 
 
 def delete_task_view(request, index):
     Task.objects.filter(id=index).update(deleted=True)
-    return HttpResponseRedirect("/all_tasks")
+    return HttpResponseRedirect("/tasks")
 
 
 def complete_task_view(request, index):
     Task.objects.filter(id=index).update(completed=True)
-    return HttpResponseRedirect("/all_tasks")
+    return HttpResponseRedirect("/tasks")
