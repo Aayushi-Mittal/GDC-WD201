0157c48d79694e699351f66ed61a811050d36f36
Aayushi Mittal <aayushimittal088@gmail.com>
2022-03-12 13:38:13 +0530

    Add more tests

--- GDC-Level-9-Milestone-master/tasks/tests.py
+++ GDC-Level-9-Milestone-master/tasks/tests.py
@@ -8,18 +8,18 @@ class TestTaskManager(TestCase):
         self.client = Client()
         self.user = User.objects.create_user(username="test", password="test123")
         self.client.login(username="testuser", password="12345")
-
-    def test_task_create(self):
-        task = Task.objects.create(
+        self.task = Task.objects.create(
             title="create test task",
             description="some description",
             priority=1,
             user=self.user,
         )
+
+    def test_task_create(self):
         self.assertTrue(
             Task.objects.filter(user=self.user, title="create test task").exists()
         )
-        self.assertEqual(str(task), "create test task")
+        self.assertEqual(str(self.task), "create test task")
 
     def test_task_read(self):
         response = self.client.get("/tasks/")
@@ -34,12 +34,29 @@ class TestTaskManager(TestCase):
         }
         response = self.client.post(f"/update-task/{self.task.id}/", temp_data)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(task.objects.first().title, "Updated test")
+        self.assertEqual(self.task.objects.first().title, "Updated test")
 
     def test_task_delete(self):
-        response = self.client.get(f"/delete-task/{self.Task.id}/")
+        response = self.client.get(f"/delete-task/{self.task.id}/")
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(Task.objects.first().deleted, True)
+        self.assertEqual(self.task.objects.first().deleted, True)
+
+    def test_task_priority_increment(self):
+        task1 = Task.objects.create(
+            title="test title",
+            description="test description",
+            priority=1,
+            user=self.user,
+        )
+        Task.objects.create(
+            title="test title",
+            description="test description",
+            priority=1,
+            user=self.user,
+        )
+        task1 = Task.objects.get(pk=task1.pk)
+        self.assertEqual(task1.priority, 1)
+
 
     def test_task_priority_does_not_increment(self):
         task1 = Task.objects.create(
@@ -57,3 +74,7 @@ class TestTaskManager(TestCase):
         )
         task1 = Task.objects.get(pk=task1.pk)
         self.assertEqual(task1.priority, 1)
+
+    def test_api(self):
+        response = self.client.get("/api/v1/task/")
+        self.assertEqual(response.status_code, 403)
